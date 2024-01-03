4efdb3e28935c16e51c2aa644f9c2a0bde6123fd
kusha2000 <kushanandarawewa1@gmail.com>
2024-01-03 19:16:32 +0530

    Place Order page created

--- scripts/customer.js
+++ scripts/customer.js
@@ -16,6 +16,7 @@ const createCustomer=()=>{
 }
 
 const loadCustomer=()=>{
+    $('#table-body').empty();
     const database=firebase.firestore();
     database.collection('customers').get().then((result)=>{
         result.forEach((records)=>{
@@ -27,8 +28,8 @@ const loadCustomer=()=>{
                 <td>${data.address}</td>
                 <td>${data.salary}</td>
                 <td>
-                    <button class="btn btn-danger btn-sm " onclick="deleteData(${records.id})">Delete</button>
-                    <button class="btn btn-success btn-sm "  onclick="updateData(${records.id})">Update</button>
+                    <button class="btn btn-danger btn-sm " onclick="deleteData('${records.id}')">Delete</button>
+                    <button class="btn btn-success btn-sm "  onclick="updateData('${records.id}')">Update</button>
                 </td>
             </tr>
             `;
@@ -36,4 +37,41 @@ const loadCustomer=()=>{
 
         })
     })
+}
+customerId=undefined;
+
+const updateData=(id)=>{
+        customerId=id;
+        const database=firebase.firestore();
+        database.collection('customers').doc(customerId).get().then((response)=>{
+            if(response.exists){
+                const data=response.data();
+                $('#name').val(data.name);
+                $('#address').val(data.address);
+                $('#salary').val(data.salary);
+            }
+        })
+}
+
+const updateRecord=()=>{
+    if(customerId){
+        const database=firebase.firestore();
+        database.collection('customers').doc(customerId).update({
+            name:$('#name').val(),
+            address:$('#address').val(),
+            salary:$('#salary').val(),
+        }).then(()=>{
+            loadCustomer();
+        })
+    }
+}
+
+const deleteData=(id)=>{
+    if(confirm('Are you Sure?')){
+        const database=firebase.firestore();
+        database.collection('customers').doc(id).delete().then(()=>{
+            toastr.success('Deleted','success!');
+            loadCustomer();
+        })
+    }
 }
---
+++ scripts/place-order.js
@@ -0,0 +1,132 @@
+let orders=[];
+
+
+const loadIds=()=>{
+    loadCustomerIds();
+    loadItemIds();
+}
+ const loadCustomerIds=()=>{
+    $('#customer-id').empty();
+    
+    const database=firebase.firestore();
+    database.collection('customers').get().then((records)=>{
+        records.forEach((results)=>{
+            const option = $('<option></option>').val(results.id).text(results.id);
+            //option.value=results.id;
+            //option.textContent=results.id;
+            $('#customer-id').append(option);
+        })
+    })
+ }
+ const loadItemIds=()=>{
+    $('#item-id').empty();
+    
+    const database=firebase.firestore();
+    database.collection('items').get().then((records)=>{
+        records.forEach((results)=>{
+            const option = $('<option></option>').val(results.id).text(results.id);
+            //option.value=results.id;
+            //option.textContent=results.id;
+            $('#item-id').append(option);
+        })
+    })
+ }
+
+ $('#customer-id').on("change",function(){
+    const customerId=$(this).val();
+    const database=firebase.firestore();
+        database.collection('customers').doc(customerId).get().then((response)=>{
+            if(response.exists){
+                const data=response.data();
+                $('#name').val(data.name);
+                $('#address').val(data.address);
+                $('#salary').val(data.salary);
+            }
+        })
+
+ })
+
+ $('#item-id').on("change",function(){
+    const itemId=$(this).val();
+    const database=firebase.firestore();
+        database.collection('items').doc(itemId).get().then((response)=>{
+            if(response.exists){
+                const data=response.data();
+                $('#description').val(data.description);
+                $('#unit-price').val(data.unitPrice);
+                $('#qty-on-hand').val(data.qtyOnHand);
+            }
+        })
+
+ })
+
+ const addToCart=()=>{
+    const unitPrice=Number.parseInt($('#unit-price').val())
+    const qty=Number.parseInt($('#qty').val())
+    const totalCost=unitPrice*qty;
+
+    const cartObj={
+        "code":$('#item-id').val(),
+        "description":$('#description').val(),
+        "unitPrice":unitPrice,
+        "qty":qty,
+        "totalCost":totalCost
+
+    };
+
+    orders.push(cartObj);
+
+    $('#cart-body').empty();
+    orders.forEach(data=>{
+        const row=`
+            <tr>
+                <td>${data.code}</td>
+                <td>${data.description}</td>
+                <td>${data.unitPrice}</td>
+                <td>${data.qty}</td>
+                <td>${data.totalCost}</td>
+            </tr>
+        `;
+        $('#cart-body').append(row);
+    });
+
+    calculateCost();
+ }
+
+const calculateCost=()=>{
+    let total=0;
+    orders.forEach(data=>{
+        total+=data.totalCost;
+    });
+    $('#net-Total').val(total);
+}
+
+const placeOrder=()=>{
+
+    const customerId=$('#customer-id').val();
+    let obj={
+        customer:{
+            customerId:customerId,
+            name:$('#name').val(),
+            address:$('#address').val(),
+            salary:$('#salary').val()
+        },
+        orderDate:new Date().toISOString().split(),
+        totalCost:Number.parseInt($('#net-Total').val()),
+        items:[]
+    }
+
+
+    
+        orders.forEach(data=>{
+            obj.items.push(data);
+        });
+        const database=firebase.firestore();
+        database.collection('orders').add(obj).then((response)=>{
+           
+            toastr.success('Saved','success!');
+        }).catch((error)=>{
+            console.log(error);
+        });
+
+}
